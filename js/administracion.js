d2be2f0613da0e8c585fb3503c122142263bee4c
MarianoGirbau <marianogirbau@gmail.com>
2023-05-17 23:15:59 -0300

    tabla responsive

--- js/administracion.js
+++ js/administracion.js
@@ -1,5 +1,7 @@
 let productos = []; //arreglo de productos
 
+
+
 class Producto {
     constructor(id,nombre,precio,descripcion,imagen) {
         this.id = id
@@ -13,6 +15,23 @@ class Producto {
 class UI {
     ShowProducts(){
         const listaProductos = document.getElementById('lista-productos')
+        listaProductos.querySelector("tbody").innerHTML = "";
+        productos.forEach((producto) => {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+            <td>${producto.descripcion}</td>
+            <td>
+            <button class="btn btn-primary edit" data-id="${producto.id}">Editar</button>
+            <button class="btn btn-danger delete" data-id="${producto.id}">Eliminar</button>
+            </td>
+            `;
+            listaProductos.querySelector("tbody").appendChild(tr);
+          });
+        
+          // Guardar los productos en el local storage
+          localStorage.setItem("productos", JSON.stringify(productos));
     }
 
     deleteProduct(){
@@ -27,16 +46,26 @@ class UI {
 //Eventos DOM
 
 //Agregar Productos
+let tabla = new UI()
 
 document.getElementById('product-form')
     .addEventListener('submit', function(event){
+    /* Obtengo los valores escritos en el formulario */    
     const nombre = document.getElementById('nombre').value
     const precio = document.getElementById('precio').value
     const descripcion = document.getElementById('descripcion').value
     const imagen = document.getElementById('imagen').value
 
-    const producto = new Producto(uuidv4(),nombre,precio,descripcion,imagen)
-    console.log(producto)
+    const producto = new Producto(uuidv4(),nombre,precio,descripcion,imagen) //Creo un objeto producto con los datos del formulario
+    
+    productos.push(producto) //agrego el producto al arreglo de productos
+    
+    console.log(productos)
+
+    tabla.ShowProducts() //Actualiza la tabla
+
+    const agregarProductosForm = document.getElementById("product-form");
+    agregarProductosForm.reset()
     
     event.preventDefault() //hace que no se actualice la pagina al enviar el form
     });
@@ -45,4 +74,14 @@ document.getElementById('product-form')
 
 function uuidv4() {
     return crypto.randomUUID();
+  }
+
+// let producto = new Producto (00,"perro",340,"sexo","www.com")
+
+// productos.push(producto)
+const productosLocalStorage = JSON.parse(localStorage.getItem("productos"));
+
+if (productosLocalStorage) {
+    productos = productosLocalStorage;
+    tabla.ShowProducts()
   }
