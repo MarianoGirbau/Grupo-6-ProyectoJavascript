c9fdd1447c3e6a91e7fee1f5e5c7733ff279a603
LicFernandez <licmartinfernandezhys@gmail.com>
2023-05-22 22:14:52 -0300

    se agregan cambios de js en carga de usuarios

--- Resgistro js/Registro.js
+++ Resgistro js/Registro.js
@@ -13,80 +13,118 @@ class Usuario {
 }
 
 class UI {
-    constructor() {
-      this.editingUserId = null;
-    }
-  
-    showProducts() {
-      const listaUsuarios = document.getElementById('lista-usuarios');
-      listaUsuarios.querySelector('tbody').innerHTML = '';
-      usuarios.forEach((Usuario) => {
-        const tr = document.createElement('tr');
-        if (Usuario.id === this.editingUserId) {
-          tr.innerHTML = `
-            <td>
-              <input type="text" id="nombre" class="form-control" value="${Usuario.nombre}" required>
-            </td>
-            <td>
-              <input type="text" id="email" class="form-control" value="${Usuario.email}">
-            </td>
-            <td>
-              <input type="password" id="contraseña" class="form-control" value="****">
-            </td>
-            <td>
-              <button class="btn btn-primary save" data-id="${Usuario.id}">Guardar</button>
-              <button class="btn btn-secondary cancel">Cancelar</button>
-            </td>
-          `;
-          tr.querySelector('.save').addEventListener('click', (event) => {
-            const userId = event.target.dataset.id;
-            const nombre = tr.querySelector('#nombre').value;
-            const email = tr.querySelector('#email').value;
-            const contraseña = tr.querySelector('#contraseña').value;
-            const usuario = usuarios.find((u) => u.id === userId);
-            if (usuario) {
-              usuario.nombre = nombre;
-              usuario.email = email;
-              usuario.contraseña = contraseña;
-              this.editingUserId = null;
-              this.showProducts(); // Actualizar la tabla
-            }
-          });
-          tr.querySelector('.cancel').addEventListener('click', () => {
+  constructor() {
+    this.editingUserId = null;
+    this.editingData = {}; // Agregamos un objeto para almacenar los datos editados temporalmente
+  }
+
+  showProducts() {
+    const listaUsuarios = document.getElementById('lista-usuarios');
+    listaUsuarios.querySelector('tbody').innerHTML = '';
+    usuarios.forEach((Usuario) => {
+      const tr = document.createElement('tr');
+      if (Usuario.id === this.editingUserId) {
+        const nombreActual = this.editingData.nombre || Usuario.nombre;
+        const emailActual = this.editingData.email || Usuario.email;
+        const contraseñaActual = this.editingData.contraseña || Usuario.contraseña;
+
+        tr.innerHTML = `
+          <td>
+            <input type="text" id="nombre-${Usuario.id}" class="form-control" value="${nombreActual}">
+          </td>
+          <td>
+            <input type="text" id="email-${Usuario.id}" class="form-control" value="${emailActual}">
+          </td>
+          <td>
+            <input type="password" id="contraseña-${Usuario.id}" class="form-control" value="****">
+          </td>
+          <td>
+            <button class="btn btn-primary save" data-id="${Usuario.id}">Guardar</button>
+            <button class="btn btn-secondary cancel" data-id="${Usuario.id}">Cancelar</button>
+          </td>
+        `;
+        
+        tr.querySelector('.save').addEventListener('click', (event) => {
+          const userId = event.target.dataset.id;
+          const nombre = document.getElementById(`nombre-${userId}`).value;
+          const email = document.getElementById(`email-${userId}`).value;
+          const contraseña = document.getElementById(`contraseña-${userId}`).value;
+          const usuario = usuarios.find((u) => u.id === userId);
+          if (usuario) {
+            usuario.nombre = nombre;
+            usuario.email = email;
+            usuario.contraseña = contraseña;
             this.editingUserId = null;
+            this.editingData = {}; // Restablecemos los datos editados temporalmente
             this.showProducts(); // Actualizar la tabla
-          });
-        } else {
-          tr.innerHTML = `
-            <td>${Usuario.nombre}</td>
-            <td>${Usuario.email}</td>
-            <td>${Usuario.contraseña}</td>
-            <td>
-              <button class="btn btn-primary edit" data-id="${Usuario.id}">Editar</button>
-              <button class="btn btn-danger delete" data-id="${Usuario.id}">Eliminar</button>
-            </td>
-          `;
-          tr.querySelector('.edit').addEventListener('click', (event) => {
-            this.editingUserId = event.target.dataset.id;
+            Swal.fire('Cambios guardados', '', 'success'); // Mostramos el mensaje de "Cambios guardados"
+          }
+        });
+
+        tr.querySelector('.cancel').addEventListener('click', (event) => {
+          const userId = event.target.dataset.id;
+          const usuario = usuarios.find((u) => u.id === userId);
+          if (usuario) {
+            if (this.editingUserId === userId) {
+              this.editingUserId = null;
+              this.editingData = {}; // Restablecemos los datos editados temporalmente
+            }
             this.showProducts(); // Actualizar la tabla
-          });
-          tr.querySelector('.delete').addEventListener('click', (event) => {
-            const userId = event.target.dataset.id;
-            const index = usuarios.findIndex((u) => u.id === userId);
-            if (index !== -1) {
-              if (this.editingUserId === userId) {
-                this.editingUserId = null;
+          }
+        });
+      } else {
+        tr.innerHTML = `
+          <td>${Usuario.nombre}</td>
+          <td>${Usuario.email}</td>
+          <td>${Usuario.contraseña}</td>
+          <td>
+            <button class="btn btn-primary edit" data-id="${Usuario.id}">Editar</button>
+            <button class="btn btn-danger delete" data-id="${Usuario.id}">Eliminar</button>
+          </td>
+        `;
+        
+        tr.querySelector('.edit').addEventListener('click', (event) => {
+          this.editingUserId = event.target.dataset.id;
+          this.editingData = {}; // Restablecemos los datos editados temporalmente
+          this.showProducts(); // Actualizar la tabla
+        });
+
+        tr.querySelector('.delete').addEventListener('click', (event) => {
+          const userId = event.target.dataset.id;
+          Swal.fire({
+            title: 'Seguro que desea borrar el usuario?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            cancelButtonText: 'Cancelar',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              const index = usuarios.findIndex((u) => u.id === userId);
+              if (index !== -1) {
+                if (this.editingUserId === userId) {
+                  this.editingUserId = null;
+                  this.editingData = {}; // Restablecemos los datos editados temporalmente
+                }
+                usuarios.splice(index, 1);
+                this.showProducts(); // Actualizar la tabla
               }
-              usuarios.splice(index, 1);
-              this.showProducts(); // Actualizar la tabla
+              Swal.fire(
+                'Eliminado!',
+                '',
+                'success'
+              );
             }
           });
-        }
-        listaUsuarios.querySelector('tbody').appendChild(tr);
-      });
-  
-      localStorage.setItem('usuarios', JSON.stringify(usuarios));
-    }
+        });
+      }
+      listaUsuarios.querySelector('tbody').appendChild(tr);
+    });
+
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+  }
 
     deleteProduct(){
 
@@ -108,21 +146,36 @@ document.getElementById('user-form')
     const email = document.getElementById('email').value
     const contraseña = document.getElementById('contraseña').value
 
+    if (nombre != "" && email != "" && contraseña != "") {
+      const usuario = new Usuario(
+        uuidv4(),
+        nombre,
+        email,
+        contraseña,
 
-    const usuario = new Usuario(uuidv4(),nombre,email,contraseña,false) 
-    
-    usuarios.push(usuario) 
-    
-    console.log(usuarios)
+      ); //Creo un objeto producto con los datos del formulario
 
-    tabla.showProducts() 
+      usuarios.push(usuario); //agrego el producto al arreglo de productos
 
+      console.log(usuarios);
 
-    const agregarUsuariosForm = document.getElementById("user-form");
-    agregarUsuariosForm.reset()
-    
-    event.preventDefault() 
-    });
+      tabla.showProducts(); //Actualiza la tabla
+
+      const agregarUsuariosForm = document.getElementById("user-form");
+
+      agregarUsuariosForm.reset();
+
+      Swal.fire("Usuario cargado correctamente!", "", "success");
+    }else {
+      
+      Swal.fire({
+        icon: 'error',
+        title: 'Algo salió mal!',
+      })
+    }
+
+    event.preventDefault(); //hace que no se actualice la pagina al enviar el form
+  });
 
 
 
