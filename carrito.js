f179a0fd42519d39030096201cc414b16c4dac77
Pablo <pablomd_98@hotmail.com>
2023-05-23 13:21:11 -0300

    avances en codigo js

--- carrito.js
+++ carrito.js
@@ -1,9 +1,27 @@
-const btnCart = document.querySelector('.container-icon')
+const btnCart = document.querySelector('.container-cart-icon')
 
-const containerCartProducts = document.querySelector('.containerCartProducts')
+const containerCartProducts = document.querySelector('.container-cart-products')
 
 btnCart.addEventListener('click' , () => {
 
     containerCartProducts.classList.toggle ('hidden-cart')
 
+})
+
+// division
+
+const cartInfo = document.querySelector('.cart-product')
+const rowProduct = document.querySelector('.row-product')
+
+// Productos a agregar
+
+const productsList = document.querySelector('.container-items')
+// .container-items es la clase de los productos a agregar
+
+// Variable de arreglos de productos
+let allProducts =[]
+
+
+productsList.addEventListener('click', e => {
+    console.log(e.target.classList.contains('btn-add-cart'))
 })
